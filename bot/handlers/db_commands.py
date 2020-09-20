5e3b03b670f2f6f7c1ca8c2bc9b0cb5c1ef5ab4b
Siberian Nerd <whatsgoin1@gmail.com>
2020-09-21 00:04:44 +0300

    add to_watchlist handler

--- bot/handlers/db_commands.py
+++ bot/handlers/db_commands.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from states import ToWatchlist, ToWatched
+from states import ToWatchlist, ToWatched, ChangeRateNote
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 import texts
@@ -8,12 +8,18 @@ from create_bot import dp
 
 
 
-@dp.message_handler(commands=['to_watchlist'])
-async def add_to_watchlist(message: types.Message):
+
+@dp.message_handler(commands=['to_watchlist'], state='*')
+async def add_to_watchlist_setup(message: types.Message):
     await message.answer(texts.TO_WATCHLIST_INFO)
-    async def to_watchlist(message: types.Message):
-        await db_sessions.add_show(message.chat.id, message.text)
-        await message.answer(f'Шоу было добавлено в ваш список.')
+    await ToWatchlist.add_name_and_type.set()
+
+
+@dp.message_handler(state=ToWatchlist.add_name_and_type, content_types=types.ContentTypes.TEXT)
+async def add_to_watchlist_main(message: types.Message, state: FSMContext):
+    await db_sessions.add_show(message.chat.id, message.text)
+    await message.answer(f'Шоу было добавлено в ваш список.')
+    await state.finish()
 
 
 """      
