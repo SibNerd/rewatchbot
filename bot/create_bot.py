6e5d1c7ba094478d347d20c17d6aa298d7e237ce
Siberian Nerd <whatsgoin1@gmail.com>
2020-09-20 23:30:59 +0300

    refractor main file

---
+++ bot/create_bot.py
@@ -0,0 +1,10 @@
+import logging
+from configs import TOKEN
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TOKEN)
+storage=MemoryStorage()
+dp = Dispatcher(bot, storage=storage)
--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,7 @@
-import logging
-from configs import TOKEN
-from aiogram import Bot, Dispatcher, executor, types
-import texts
+from aiogram import executor
+from create_bot import dp
+import handlers
 
-API_TOKEN = TOKEN
-
-logging.basicConfig(level=logging.INFO)
-
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
-
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    await message.reply(texts.HELP_INFO)
-
-
+#main function
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
-    
