c11ce414351ceb48dc1f4b77cb3b94152b40a020
Siberian Nerd <whatsgoin1@gmail.com>
2020-09-20 23:26:21 +0300

    add database structure

---
+++ bot/db.py
@@ -0,0 +1,22 @@
+import sqlalchemy as sa
+from sqlalchemy import (
+    Table, MetaData, ForeignKey, Column, Integer, Boolean, String, Text, DateTime
+    )
+metadata = MetaData()
+
+users = Table(
+    'users', metadata,
+    Column('user_id', Integer, primary_key=True),
+    Column('name', String, nullable=False)
+    )
+
+shows = Table(
+    'shows', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('user_id', Integer, ForeignKey('users.user_id', ondelete="CASCADE")),
+    Column('name', String, nullable=False),
+    Column('type', String, nullable=False),
+    Column('is_watched', Boolean, default=False),
+    Column('rate', Integer, default=None),
+    Column('note', Text, default=None)
+    )
