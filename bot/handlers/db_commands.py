dc99abc15e4b805eec64971010e30a739c44d299
Siberian Nerd <whatsgoin1@gmail.com>
2020-09-22 21:16:13 +0300

    add handlers for getting rates and notes

--- bot/handlers/db_commands.py
+++ bot/handlers/db_commands.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from states import ToWatchlist, ToWatched, ChangeRateNote
+from states import ToWatchlist, ToWatched, GetRate, GetNote
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Text
 import texts
@@ -10,32 +10,32 @@ import re
 
 @dp.message_handler(commands=['to_watchlist'], state='*')
 async def add_to_watchlist_setup(message: types.Message):
-    await message.answer(texts.TO_WATCHLIST_INFO)
+    await message.answer(texts.INFO_SETUP)
     await ToWatchlist.add_name_and_type.set()
 
 @dp.message_handler(state=ToWatchlist.add_name_and_type, content_types=types.ContentTypes.TEXT)
 async def add_to_watchlist_main(message: types.Message, state: FSMContext):
     user_data = re.split(r'\n', message.text)
     if len(user_data) == 2:
-        await db_sessions.add_show(message.chat.id, message.text)
+        await db_sessions.add_show(message.chat.id, user_data)
         await message.answer('Шоу было добавлено в ваш список.')
         await state.finish()
     else:
-        await message.answer(texts.TO_WATCHLIST_INFO)
+        await message.answer(texts.INFO_SETUP)
         return
 
 
     
 @dp.message_handler(commands=['watched'], state='*')
 async def add_to_watched_setup(message: types.Message):
-    await message.answer(texts.TO_WATCHED_INFO_SETUP)
+    await message.answer(texts.INFO_SETUP)
     await ToWatched.add_name_and_type.set()
 
 @dp.message_handler(state=ToWatched.add_name_and_type, content_types=types.ContentTypes.TEXT)
 async def add_to_watched_nametype(message: types.Message, state: FSMContext):
     user_data = re.split('\n', message.text)
     if len(user_data) == 2:
-        await db_sessions.add_to_watched(message.chat.id, message.text)
+        await db_sessions.add_to_watched(message.chat.id, user_data)
         await state.update_data(show_name_type=message.text)
         await ToWatched.next()
         await message.answer(texts.TO_WATCHED_INFO_RATE_NOTE)
@@ -86,3 +86,37 @@ async def add_to_watched_note(message: types.Message, state: FSMContext):
         await db_sessions.add_note(message.chat.id, user_data['show_name_type'], message.text)
         await state.finish()
     await message.answer('Шоу было добавлено в просмотренные.')
+
+
+
+@dp.message_handler(commands=['get_note'], state='*')
+async def get_show_note_setup(message: types.Message):
+    await message.answer(texts.INFO_SETUP)
+    await GetNote.get_note.set()
+
+@dp.message_handler(state=GetNote.get_note, content_types=types.ContentTypes.TEXT)
+async def get_show_note(message: types.Message, state: FSMContext):
+    user_data = re.split(r'\n', message.text)
+    if len(user_data) == 2:
+        note = await db_sessions.get_show_note(message.chat.id, user_data)
+        await message.answer(note)
+        await state.finish()
+    else:
+        await message.answer(texts.INFO_SETUP)
+
+
+
+@dp.message_handler(commands=['get_rate'], state="*")
+async def get_show_rate_setup(message: types.Message):
+    await message.answer(texts.INFO_SETUP)
+    await GetRate.get_rate.set()
+
+@dp.message_handler(state=GetRate.get_rate, content_types=types.ContentTypes.TEXT)
+async def get_show_rate(message: types.Message, state: FSMContext):
+    user_data = re.split(r'\n', message.text)
+    if len(user_data) == 2:
+        rate = await db_sessions.get_show_rate(message.chat.id, user_data)
+        await message.answer(rate)
+        await state.finish()
+    else:
+        await message.answer(texts.INFO_SETUP)
