654f629a5951db8c23d32447c69dfe4b7e8f7fc7
Siberian Nerd <54741988+SibNerd@users.noreply.github.com>
2020-12-27 14:01:31 +0300

    change db connections
    connects on startup and disconnects on shutdown

--- bot/db_sessions.py
+++ bot/db_sessions.py
@@ -2,12 +2,8 @@
 Module with bot functions.
 """
 
-from databases import Database
+from main import database as db
 from db import users, shows
-import re
-import os
-
-DATABASE_URL = os.environ['DATABASE_URL']
 
 
 
@@ -21,16 +17,15 @@ async def add_new_user(new_user_id, name):
     Returns:
         bool: True if user was added to DB, False otherwise
     """
-    async with Database(DATABASE_URL) as db:
-        query = "SELECT * FROM users WHERE user_id = :user_id"
-        result = await db.fetch_one(query=query, values={'user_id': new_user_id})
-        if not result:
-            query = users.insert()
-            values = {'user_id': new_user_id, 'name': name}
-            add_user = await db.execute(query=query, values=values)
-            return True
-        else:
-            return False
+    query = "SELECT * FROM users WHERE user_id = :user_id"
+    result = await db.fetch_one(query=query, values={'user_id': new_user_id})
+    if not result:
+        query = users.insert()
+        values = {'user_id': new_user_id, 'name': name}
+        add_user = await db.execute(query=query, values=values)
+        return True
+    else:
+        return False
     
 
 
@@ -42,10 +37,9 @@ async def add_show(user_id, user_message):
         user_message (str): Name and Type of the show
     """
     show_name, show_type = user_message
-    async with Database(DATABASE_URL) as db:
-        query = shows.insert()
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
-        await db.execute(query=query, values=values)
+    query = shows.insert()
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'is_watched': False}
+    await db.execute(query=query, values=values)
         
 
 
@@ -57,17 +51,16 @@ async def add_to_watched(user_id, user_message):
         user_message (str): Name and Type of the show
     """
     show_name, show_type = user_message
-    async with Database(DATABASE_URL) as db:
-        query = "SELECT * FROM shows WHERE user_id = :user_id AND name = :name AND type = :type"
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
-        result = await db.fetch_one(query=query, values=values)
-        if not result:
-            query = shows.insert()
-            values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'is_watched': True}
-        else:
-            query = "UPDATE shows SET is_watched = :is_watched WHERE user_id = :user_id AND name = :name AND type = :type"
-            values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'is_watched': True}
-        await db.execute(query=query, values=values)
+    query = "SELECT * FROM shows WHERE user_id = :user_id AND name = :name AND type = :type"
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
+    result = await db.fetch_one(query=query, values=values)
+    if not result:
+        query = shows.insert()
+        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'is_watched': True}
+    else:
+        query = "UPDATE shows SET is_watched = :is_watched WHERE user_id = :user_id AND name = :name AND type = :type"
+        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'is_watched': True}
+    await db.execute(query=query, values=values)
         
 
 
@@ -80,13 +73,12 @@ async def get_watchlist(user_id):
     Returns:
         list[str]: list of all shows in User's watchlist
     """
-    async with Database(DATABASE_URL) as db:
-        query = 'SELECT name, type FROM shows WHERE user_id = :user_id AND is_watched = :is_watched'
-        values = {'user_id': user_id, 'is_watched': False}
-        result = await db.fetch_all(query=query, values=values)
-        if not result:
-            result = 'Ваш список желаемого к просмотру пуст.'
-        return result
+    query = 'SELECT name, type FROM shows WHERE user_id = :user_id AND is_watched = :is_watched'
+    values = {'user_id': user_id, 'is_watched': False}
+    result = await db.fetch_all(query=query, values=values)
+    if not result:
+        result = 'Ваш список желаемого к просмотру пуст.'       
+    return result
 
 
 
@@ -99,10 +91,9 @@ async def add_show_rate(user_id, name_type, rate):
         rate (int): Rate of the show
     """
     show_name, show_type = name_type
-    async with Database(DATABASE_URL) as db:
-        query = 'UPDATE shows SET rate = :rate WHERE user_id = :user_id AND name = :name AND type = :type'
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'rate': rate}
-        await db.execute(query=query, values=values)
+    query = 'UPDATE shows SET rate = :rate WHERE user_id = :user_id AND name = :name AND type = :type'
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'rate': rate}
+    await db.execute(query=query, values=values)
     
 
 
@@ -115,10 +106,9 @@ async def add_show_note(user_id, name_type, note):
         note (str): Note for the show
     """
     show_name, show_type = name_type
-    async with Database(DATABASE_URL) as db:
-        query = 'UPDATE shows SET note = :note WHERE user_id = :user_id AND name = :name AND type = :type'
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'note': note}
-        await db.execute(query=query, values=values)
+    query = 'UPDATE shows SET note = :note WHERE user_id = :user_id AND name = :name AND type = :type'
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type, 'note': note}
+    await db.execute(query=query, values=values)
 
 
 
@@ -133,13 +123,12 @@ async def get_show_rate(user_id, user_message):
         result: rate of the chosen show of chosen User or text, that chosen show doesn't have a rate
     """
     show_name, show_type = user_message
-    async with Database(DATABASE_URL) as db:
-        query = 'SELECT rate FROM shows WHERE user_id = :user_id AND name = :name AND type = :type'
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
-        result = await db.fetch_one(query=query, values=values)
-        if not result:
-            result = 'У данного шоу нет оценки.'
-        return result[0]
+    query = 'SELECT rate FROM shows WHERE user_id = :user_id AND name = :name AND type = :type'
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
+    result = await db.fetch_one(query=query, values=values)
+    if not result:
+        result = 'У данного шоу нет оценки.'
+    return result[0]
 
 
 
@@ -153,20 +142,19 @@ async def get_show_average_rate(user_message):
         arate: average rate of the show or text, that chosen show doesn't have any rates.
     """
     show_name, show_type = user_message
-    async with Database(DATABASE_URL) as db:
-        query = 'SELECT rate FROM shows WHERE name = :name AND type = :type'
-        values = {'name': show_name, 'type': show_type}
-        result = db.fetch_all(query=query, values=values)
-        if not result:
-            arate = 'У данного шоу нет оценок.'
-        elif len(result)>1:
-            arate = 0
-            for i in result:
-                arate += i
-            arate = int(arate/len(result))
-        else:
-            arate = result[0]
-        return arate
+    query = 'SELECT rate FROM shows WHERE name = :name AND type = :type'
+    values = {'name': show_name, 'type': show_type}
+    result = db.fetch_all(query=query, values=values)
+    if not result:
+        arate = 'У данного шоу нет оценок.'
+    elif len(result)>1:
+        arate = 0
+        for i in result:
+            arate += i
+        arate = int(arate/len(result))
+    else:
+        arate = result[0]
+    return arate
 
 
 
@@ -181,11 +169,10 @@ async def get_show_note(user_id, user_message):
         result: User's note
     """
     show_name, show_type = user_message
-    async with Database(DATABASE_URL) as db:
-        query = 'SELECT note FROM shows WHERE user_id = :user_id AND name = :name AND type = :type'
-        values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
-        result = await db.fetch_one(query=query, values=values)
-        if not result:
-            result = 'У данного шоу нет заметки.'
-        return result[0]
+    query = 'SELECT note FROM shows WHERE user_id = :user_id AND name = :name AND type = :type'
+    values = {'user_id': user_id, 'name': show_name.lower(), 'type': show_type}
+    result = await db.fetch_one(query=query, values=values)
+    if not result:
+        result = 'У данного шоу нет заметки.'
+    return result[0]
 
--- bot/main.py
+++ bot/main.py
@@ -1,7 +1,20 @@
+from create_bot import dp, DATABASE_URL
 from aiogram import executor
-from create_bot import dp
+from databases import Database
 import handlers
+
+database = Database(DATABASE_URL)
+
+async def on_startup(db):
+    await database.connect()
+
+async def on_shutdown(dp):
+    await database.disconnect()
 
 #main function
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(
+        dp, 
+        skip_updates=True, 
+        on_startup=on_startup, 
+        on_shutdown=on_shutdown)
