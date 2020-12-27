16e2e51e52bde19d9638c4c9d902e52f64ed01ab
Siberian Nerd <54741988+SibNerd@users.noreply.github.com>
2020-12-27 14:00:05 +0300

    add database_url to bot configs

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -10,6 +10,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = os.environ['TOKEN']
+DATABASE_URL = os.environ['DATABASE_URL']
 
 bot = Bot(token=TOKEN)
 storage=MemoryStorage()
